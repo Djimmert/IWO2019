4be54e8e9c5aa7516130e7003bd47d32140d9039
Djimmert <d.casander@outlook.com>
2020-03-10 15:18:35 +0100

    Corrected code to working bash

--- wiki_count_de.sh
+++ wiki_count_de.sh
@@ -4,7 +4,7 @@
 #
 # Usage: ./wiki_count_de.sh
 
-URL = "https://nl.wikipedia.org/wiki/Rijksuniversiteit_Groningen?action=raw"
+URL="https://nl.wikipedia.org/wiki/Rijksuniversiteit_Groningen?action=raw"
 
 # 
-curl -s $URL | grep -iow 'de' | wc -l
+curl -s "$URL" | grep -iow 'de' | wc -l
