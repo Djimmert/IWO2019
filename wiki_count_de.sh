96f821a3ad7570134a8cbc05dc8ff1482ac95b8d
Djimmert <d.casander@outlook.com>
2020-03-10 15:10:05 +0100

    Add description of file

--- wiki_count_de.sh
+++ wiki_count_de.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# Description:
+# Description: output the frequency of the word 'de' in the article specified below.
+#              In this case it is the Dutch wikipedia page on the RuG.
 #
 # Usage: ./wiki_count_de.sh
 
+URL = "https://nl.wikipedia.org/wiki/Rijksuniversiteit_Groningen?action=raw"
+
 # 
-curl -s https://nl.wikipedia.org/wiki/Rijksuniversiteit_Groningen?action=raw | grep -iow 'de' | wc -l
+curl -s $URL | grep -iow 'de' | wc -l
