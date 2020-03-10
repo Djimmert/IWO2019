635648771a7ddfa1ee54d7510558f3c72ae9f950
Djimmert <d.casander@outlook.com>
2020-03-10 15:27:58 +0100

    Added description for code lines

--- wiki_count_de.sh
+++ wiki_count_de.sh
@@ -4,7 +4,8 @@
 #
 # Usage: ./wiki_count_de.sh
 
+# Insert the URL where 'de' will be counted
 URL="https://nl.wikipedia.org/wiki/Rijksuniversiteit_Groningen?action=raw"
 
-# 
+# First, get the online text from the url, secondly prints a line for all instances of 'de', then counts those lines.
 curl -s "$URL" | grep -iow 'de' | wc -l
